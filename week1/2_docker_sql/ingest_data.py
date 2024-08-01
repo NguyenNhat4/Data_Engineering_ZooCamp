90c49b9a3f32e754f43d384c86a3986fb825dcfe
nhatnguyen <minhnhatzzsi4@gmail.com>
2024-08-01 13:39:56 +0700

    fix some bugs

--- week1/2_docker_sql/ingest_data.py
+++ week1/2_docker_sql/ingest_data.py
@@ -18,16 +18,19 @@ def main(params):
     url = params.url
     csv_name = 'output.csv'
     print("downloading data from source....\n")
+    t_start = time()
     urllib.request.urlretrieve(url, csv_name)
-
+    t_end = time()
+    print('downloading just completed after %.3f seconds.' % (t_end-t_start))
 
     # os.system(f"wget {url} -O {csv_name}")
     engine = create_engine(f'postgresql://{username}:{password}@{host}:{port}/{db}')
-    engine.connect
     df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
     print("read csv....\n")
 
     df = next(df_iter)
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
     df.head(n=0).to_sql(name=table_name , con=engine,if_exists='replace')  
     df.to_sql(name=table_name, con=engine, if_exists='append')
     print("Start ingresting data into database ...\n")
